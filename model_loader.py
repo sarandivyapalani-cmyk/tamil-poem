4714d718c764d8dc9e48486183a765f695361223
sarandivyapalani-cmyk <sarandivyapalani@gmail.com>
2026-02-13 16:26:59 +0530

    Update model_loader.py

--- model_loader.py
+++ model_loader.py
@@ -1,21 +1,22 @@
+import streamlit as st
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 
-# Smaller working model
-model_name = "ai4bharat/indictrans2-indic-indic-dist-200M"
+@st.cache_resource
+def load_model():
+    model_name = "ai4bharat/indictrans2-indic-indic-dist-200M"
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
+    return tokenizer, model
 
-tokenizer = AutoTokenizer.from_pretrained(model_name)
-model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
+tokenizer, model = load_model()
 
 def convert_to_modern_tamil(text):
 
-    # prompt instruction
     input_text = f"Convert classical Tamil to modern Tamil: {text}"
 
     inputs = tokenizer(input_text, return_tensors="pt", padding=True)
-
     outputs = model.generate(**inputs, max_length=256)
 
-    result = tokenizer.decode(outputs[0], skip_special_tokens=True)
+    return tokenizer.decode(outputs[0], skip_special_tokens=True)
 
-    return result
 
