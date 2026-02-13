2cf6f005f374717b5a2686f76a299e3b1be445be
sarandivyapalani-cmyk <sarandivyapalani@gmail.com>
2026-02-13 16:25:39 +0530

    Update model_loader.py

--- model_loader.py
+++ model_loader.py
@@ -1,20 +1,21 @@
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 
-# Load IndicTrans2 model (Tamil support)
-model_name = "ai4bharat/indictrans2-en-indic-1B"
+# Smaller working model
+model_name = "ai4bharat/indictrans2-indic-indic-dist-200M"
 
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
 
 def convert_to_modern_tamil(text):
 
-    # Prompt telling AI what to do
+    # prompt instruction
     input_text = f"Convert classical Tamil to modern Tamil: {text}"
 
     inputs = tokenizer(input_text, return_tensors="pt", padding=True)
 
-    outputs = model.generate(**inputs, max_length=200)
+    outputs = model.generate(**inputs, max_length=256)
 
     result = tokenizer.decode(outputs[0], skip_special_tokens=True)
 
     return result
+
